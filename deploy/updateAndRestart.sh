a5760264fb796ce5be327ecee60ebff866f607a5
Guru Charan <gururk2@gmail.com>
2020-07-23 13:33:10 +0000

    Update updateAndRestart.sh

--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -4,10 +4,10 @@
 set -e
 
 # Delete the old repo
-rm -rf /home/mindfinadmin/mindfin_website
+rm -rf /home/anantaloop/mindfin/mindfin_website
  
 # clone the repo again
-git clone git@gitlab.com:mindfin1/mindfin_website.git /home/mindfinadmin/mindfin_website 
+git clone git@gitlab.com:mindfin1/mindfin_website.git /home/anantaloop/mindfin/mindfin_website 
 
 
 
